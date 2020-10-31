fc5944b3ccf0439dde8d15b48db10ece3b1ba419
Ivan Dobry <ivandobry@hotmail.es>
2020-10-31 18:33:57 +0100

    more slider

--- src/components/details/SliderDetail.jsx
+++ src/components/details/SliderDetail.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import store from '../../stores/principal-store';
+import React, { useEffect} from 'react';
 import './SliderDetail.css';
 
 
-function SliderDetail ()  {
+function SliderDetail ({animal})  {
 
     useEffect(() => {
+      if(animal) {
         showSlides(slideIndex);
+      }
     })
 
     let slideIndex = 1;
@@ -44,34 +45,27 @@ function SliderDetail ()  {
     
     return (
     <>
+    
     <div className='slide'>
         <aside className='slide__aside aside-left' onClick={() => plusSlides(-1)}><span className="material-icons">
                 chevron_left
             </span></aside>
-        <div className="mySlides fade">
-            <img className='slide__image' alt=''
-                src="https://trello-attachments.s3.amazonaws.com/5f9bf9da4ef72b5e96d70d02/5f9c013b390a4735863ccb17/6ccf02c311640e972469842d9ec378bc/1.jpg"/>
-        </div>
-        <div className="mySlides fade">
-            <img className='slide__image' alt=''
-                src="https://trello-attachments.s3.amazonaws.com/5f9bf9da4ef72b5e96d70d02/5f9c013b390a4735863ccb17/d9441113b6f3a47de80803ce3fd867ce/2.jpg"/>
-        </div>
-        <div className="mySlides fade">
-            <img className='slide__image' alt=''
-                src="https://trello-attachments.s3.amazonaws.com/5f9bf9da4ef72b5e96d70d02/5f9c013b390a4735863ccb17/24970a8654d78cbfc1455460eb718bc5/3.jpg"/>
-        </div>
-        <div className="mySlides fade">
-            <img className='slide__image' alt=''
-                src="https://trello-attachments.s3.amazonaws.com/5f9bf9da4ef72b5e96d70d02/5f9c013b390a4735863ccb17/ed7031f14f6131905294fe18fc542aea/4.jpg"/>
-        </div>
+            {animal?.photos.map((photo) => {
+      return (
+     <div className="mySlides fade" key={animal?.photo} >
+      <img className='slide__image' alt='' key={animal?.photo}
+          src={`${photo.full}`}/>
+     </div>)
+    })}
         <aside className='slide__aside aside-right' onClick={() => plusSlides(1)}><span className="material-icons">
                 chevron_right
             </span></aside>
         <ul className='slide__dots'>
-            <li className='dot' onClick={() => currentSlide(1)}></li>
-            <li className='dot' onClick={() => currentSlide(2)}></li>
-            <li className='dot' onClick={() => currentSlide(3)}></li>
-            <li className='dot' onClick={() => currentSlide(4)}></li>
+            {
+            animal?.photos.map((element, index) => {
+              return <li className='dot' key={index + 1} onClick={() => currentSlide(index + 1)}></li>
+            })
+            }
         </ul>
     </div>
     </>
