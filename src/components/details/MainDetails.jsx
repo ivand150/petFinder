6b0bbce69d6090cc66aa9b1565b4154174931045
Jheavi <javierlaso213@hotmail.com>
2020-10-31 14:12:46 +0100

    fixed conflicts

--- src/components/details/MainDetails.jsx
+++ src/components/details/MainDetails.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './MainDetails.css';
 import Details from './Details';
+import SliderDetail from './SliderDetail';
 
 function MainDetails() {
     return (
         <main>
+            <SliderDetail />
             <Details />
         </main>
     );
