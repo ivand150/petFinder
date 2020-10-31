d3f36803cb5db12db225e1642da5a95d68447cc5
Jheavi <javierlaso213@hotmail.com>
2020-10-31 13:04:13 +0100

    added logos and header color

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,8 +7,9 @@ function Header() {
         <header className="header">
             <BurgerButton />
             <img
-                src="https://trello-attachments.s3.amazonaws.com/5f9bf9da4ef72b5e96d70d02/5f9d3d8395a20040a815a80a/5c4e9dda49a0f86d10b237adcbb43c78/584eb97de7011_thumb80.jpg"
-                alt=""
+                className="header__logo"
+                src="https://trello-attachments.s3.amazonaws.com/5f9d3d8395a20040a815a80a/768x727/6b9e6b190008963e1377a82e5497b4c1/dogcat2.png"
+                alt="logo"
             />
             <div className="search">
                 <span id="search__icon" className="material-icons">
--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/header/Header';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Footer from './components/footer/Footer';
 
 ReactDOM.render(
     <React.StrictMode>
         <Header />
-        <Footer />
         <BrowserRouter></BrowserRouter>
+        <Footer />
     </React.StrictMode>,
     document.getElementById('root')
-
 );
 
 // If you want to start measuring performance in your app, pass a function
