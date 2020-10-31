8bee0f84fc137fb9788bcddf78694d2a22f741d2
Ivan Dobry <ivandobry@hotmail.es>
2020-10-31 14:04:33 +0100

    slider

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import Header from './components/header/Header';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/header/Header";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Footer from "./components/footer/Footer";
+import SliderDetail from "./components/slider-detail/SliderDetail";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Header />
-        <Footer />
-        <BrowserRouter></BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
-
+  <React.StrictMode>
+    <Header />
+    <SliderDetail />
+    <Footer />
+    <BrowserRouter></BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
