c01a2b3c077c98e0fbf6983638e4a3699acae5c2
Jheavi <javierlaso213@hotmail.com>
2020-10-31 11:56:41 +0100

    added burgerbutton, logo, search

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,9 +4,18 @@ import './Header.css';
 
 function Header() {
     return (
-        <header>
+        <header className="header">
             <BurgerButton />
-            <input type="text" />
+            <img
+                src="https://trello-attachments.s3.amazonaws.com/5f9bf9da4ef72b5e96d70d02/5f9d3d8395a20040a815a80a/5c4e9dda49a0f86d10b237adcbb43c78/584eb97de7011_thumb80.jpg"
+                alt=""
+            />
+            <div className="search">
+                <span id="search__icon" className="material-icons">
+                    search
+                </span>
+                <input id="search__input" type="text" />
+            </div>
         </header>
     );
 }
