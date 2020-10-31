8bbd34936f07d37073e4b80dd443f83abcb568a6
Ivan Dobry <ivandobry@hotmail.es>
2020-10-31 12:24:26 +0100

    footer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Footer from "./components/footer/Footer";
 
 ReactDOM.render(
-    <React.StrictMode></React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Footer />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
