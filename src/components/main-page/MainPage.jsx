f125265e400266d4ae93995724bd07f2e13beacb
Jheavi <javierlaso213@hotmail.com>
2020-11-01 13:28:43 +0100

    added css

--- src/components/main-page/MainPage.jsx
+++ src/components/main-page/MainPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './MainPage.css';
 
 function MainPage() {
     return (
         <main>
-            <Link to={`list/dog`}>Find a Dog</Link>
+            <Link to={`list/dog`} className="principal-button">
+                Find a Dog
+            </Link>
         </main>
     );
 }
