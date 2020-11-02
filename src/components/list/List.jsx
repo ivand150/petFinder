4a10076d47d46bfaf733f016a8f867259a56bf05
Jheavi <javierlaso213@hotmail.com>
2020-11-02 23:27:13 +0100

    created list with cards

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,38 +1,44 @@
-import React from "react";
-
-import "./List.css";
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import './List.css';
 
 function List({ animals }) {
   return (
     <>
       <section className="main-list-container">
-        <ul className="list-pets">
-          {" "}
-          {animals?.map((animal) => {
+        <ul className="list-pets d-flex flex-sm-column align-items-center flex-md-row flex-md-wrap">
+          {' '}
+          {animals?.map((animal, index) => {
             return (
-              <li>
-                <div className="pet-card">
-                  <img
-                    className="pet-card__image"
-                    src={animal.photos[0]?.medium}
-                    alt=""
-                  />
-                  <div>
-                    <div className="pet-card__name">
-                      <span className="name__pet-name">{animal.name}</span>
+              <li
+                key={index}
+                className="animal-card d-flex justify-content-center"
+              >
+                <Card className="m-3" style={{ width: '100%' }}>
+                  <Card.Body>
+                    <div className="d-flex flex-column align-items-center">
+                      <Card.Img
+                        variant="top"
+                        src={
+                          !animal.photos[0]
+                            ? 'https://trello-attachments.s3.amazonaws.com/5f9c013b390a4735863ccb17/450x450/e6580d415bd4c35ae516c6555b294afb/112815953-stock-vector-no-image-available-icon-flat-vector.jpg'
+                            : animal.photos[0].medium
+                        }
+                      />
+                      <Card.Title>{animal.name}</Card.Title>
                     </div>
-                    <div className="pet-card__details">
-                      <div className="pet-card__age">
-                        <span className="age__pet-age">{animal.age}</span>
+                    <div className="d-flex justify-content-around">
+                      <div className="d-flex flex-column attributes">
+                        <Card.Text>{animal.type}</Card.Text>
+                        <Card.Text>{animal.breeds.primary}</Card.Text>
                       </div>
-                      <div className="pet-card__breed">
-                        <span className="breed__pet-breed">
-                          {animal.breeds.primary}
-                        </span>
+                      <div className="d-flex flex-column attributes">
+                        <Card.Text>{animal.gender}</Card.Text>
+                        <Card.Text>{animal.age}</Card.Text>
                       </div>
                     </div>
-                  </div>
-                </div>
+                  </Card.Body>
+                </Card>
               </li>
             );
           })}
