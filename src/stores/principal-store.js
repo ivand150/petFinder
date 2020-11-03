f928885192585bd5c4f1addd7818b2d30ef47dba
Jheavi <javierlaso213@hotmail.com>
2020-11-03 19:09:22 +0100

    fixed issue 3

--- src/stores/principal-store.js
+++ src/stores/principal-store.js
@@ -1,82 +1,82 @@
-import { EventEmitter } from "events";
-import dispatcher from "../dispatcher/dispatcher";
-import actionTypes from "../actions/action-types";
+import { EventEmitter } from 'events';
+import dispatcher from '../dispatcher/dispatcher';
+import actionTypes from '../actions/action-types';
 
-const CHANGE = "CHANGE";
+const CHANGE = 'CHANGE';
 let _animal;
 let _token =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJEMFZNYzJ3a0tVTEVkNzNtQkpoSWhWazdqaVUxcXgwVWpXbnFuWWZQTmRrdWMxT0VRUiIsImp0aSI6IjBhOTNkNWMxNTZlMzVlNzIwZjczZmJmNGU1MDVhODZjNjY4OTNjNjJlNTI2ZjExY2FmYjViNzVmNTEzMWUxMWUzNzYzZDg5OWVjZjNiZjg0IiwiaWF0IjoxNjA0Mzk0Nzk3LCJuYmYiOjE2MDQzOTQ3OTcsImV4cCI6MTYwNDM5ODM5Niwic3ViIjoiIiwic2NvcGVzIjpbXX0.se772Ph371Q4spcPnDx0HligGlQldbihrHtdhkHykWC9h1M28YpMD8ejJwcYowebw54fItfp6uzoXDnO2Y96FyX45wnKD29stBByD96je66uxXLCOrqlGPwKFuzHXwQErU9qFyKyD6Zd6VP2vBreLGJqeqfHHoQkANs4VntLL3EO9XF9k8lIAKQIxe0z88huIJ8ZaHQndff1gZdHt_wNkuHVptwkj-fMbJKGqMwamRX0GVy9jjrwubUMdzebefkd_Sojdt7B_ZjmQiD9x3rRcmRbX96RCmY3VB7uFoCgEa3TGXj4CY8q0qPG0ePO4cEdxfX4vF3Kb7lrmnO0dFLzwA";
+	'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJEMFZNYzJ3a0tVTEVkNzNtQkpoSWhWazdqaVUxcXgwVWpXbnFuWWZQTmRrdWMxT0VRUiIsImp0aSI6ImE2YjU3NDc0YjlmY2I1ODMwNDFiNThiY2FmNDE2MjhkNzZjMjZiMmI0Yzg3YmU2NzQzMDFkZjU3ZjI4ZmIzNjZkMzMwYTA4MmQwYjg3ODM3IiwiaWF0IjoxNjA0NDI1OTU5LCJuYmYiOjE2MDQ0MjU5NTksImV4cCI6MTYwNDQyOTU1OSwic3ViIjoiIiwic2NvcGVzIjpbXX0.RIHZnw8oPrEyzyBJJ0pxfhnED5B17WRN28lnLGppD1pTSzZbELMLdwTRIKXpCgVvB_6QgAt1irvmt1bhEXbChYZ8ifs-cBVV4CfIhc5LBv8AnVcP9JcOv4hWMo1Dj34Hyb_zXHxxefqmVISthvJh1n0x3gzfJPftciTPvTtKJgyJEuFzmjv9EE26q1GqkGogYmfLBrSTJSHA31opswIyRhfZU6bUMTKW-0vHGZz_W5lTiV4NaHYvQYyTOTzSbE6n5UM-f3AT8Nv-lYYsp5LC9Vkv1onT1dLyPPmVxOOmLpUpE-cPx_gO2Tdtc0LbpI-RVrlskOWGJlzwmKAlhqhmHg';
 let _animals = [];
 let _test;
 
 class Store extends EventEmitter {
-  setToken(token) {
-    _token = token;
-  }
+	setToken(token) {
+		_token = token;
+	}
 
-  getToken() {
-    return _token;
-  }
+	getToken() {
+		return _token;
+	}
 
-  setAnimal(animal) {
-    _animal = animal;
-  }
+	setAnimal(animal) {
+		_animal = animal;
+	}
 
-  getAnimal() {
-    return _animal;
-  }
+	getAnimal() {
+		return _animal;
+	}
 
-  setAnimals(animals) {
-    _animals = animals;
-  }
+	setAnimals(animals) {
+		_animals = animals;
+	}
 
-  getAnimals() {
-    return _animals;
-  }
+	getAnimals() {
+		return _animals;
+	}
 
-  removeLastComma(str) {
-    return str.slice(-1) === "," ? str.slice(0, str.length - 1) : str;
-  }
+	removeLastComma(str) {
+		return str.slice(-1) === ',' ? str.slice(0, str.length - 1) : str;
+	}
 
-  addEventListener(callback) {
-    this.on(CHANGE, callback);
-  }
+	addEventListener(callback) {
+		this.on(CHANGE, callback);
+	}
 
-  removeEventListener(callback) {
-    this.removeListener(CHANGE, callback);
-  }
+	removeEventListener(callback) {
+		this.removeListener(CHANGE, callback);
+	}
 
-  emitChange() {
-    this.emit(CHANGE);
-  }
+	emitChange() {
+		this.emit(CHANGE);
+	}
 
-  getTestVar() {
-    return _test;
-  }
+	getTestVar() {
+		return _test;
+	}
 }
 
 const store = new Store();
 
 dispatcher.register((action) => {
-  switch (action.type) {
-    case actionTypes.REQUEST_TOKEN:
-      _token = action.payload;
-      store.emitChange();
-      break;
-    case actionTypes.REQUEST_ANIMAL:
-      _animal = action.payload;
-      store.emitChange();
-      break;
-    case actionTypes.REQUEST_ANIMALS:
-      _animals = action.payload;
-      store.emitChange();
-      break;
+	switch (action.type) {
+		case actionTypes.REQUEST_TOKEN:
+			_token = action.payload;
+			store.emitChange();
+			break;
+		case actionTypes.REQUEST_ANIMAL:
+			_animal = action.payload;
+			store.emitChange();
+			break;
+		case actionTypes.REQUEST_ANIMALS:
+			_animals = action.payload;
+			store.emitChange();
+			break;
 
-    default:
-      _test = "break";
-      store.emitChange();
-      break;
-  }
+		default:
+			_test = 'break';
+			store.emitChange();
+			break;
+	}
 });
 
 export default store;
