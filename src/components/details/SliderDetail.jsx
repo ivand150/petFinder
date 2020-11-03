af2da390d41a2c0b2a1f4af7dfe32fdf437764ad
Ivan Dobry <ivandobry@hotmail.es>
2020-11-03 10:08:38 +0100

    slider styling

--- src/components/details/SliderDetail.jsx
+++ src/components/details/SliderDetail.jsx
@@ -4,24 +4,40 @@ import Carousel from 'react-bootstrap/Carousel';
 
 
 function SliderDetail ({animal})  {
-debugger
-  return(
-    <> 
+  if (!animal?.photos || animal?.photos.length < 1) {
+    return(
+      <>
       <Carousel>
-      {animal?.photos.map((photo) =>{
-        return(
-        <Carousel.Item>
-          <img
-          className="d-block w-100"
-          src={`${photo.full}`}
-          alt="First slide"
-        />
-        </Carousel.Item> 
-       );
-      })};
-      </Carousel>
-    </>
-  ); 
+      <Carousel.Item>
+    <img
+      className="d-block w-100 slide__image"
+      src="https://trello-attachments.s3.amazonaws.com/5f9bf9da4ef72b5e96d70d02/5f9c013b390a4735863ccb17/649f0410e7a316f5720a9deaea73b588/112815953-stock-vector-no-image-available-icon-flat-vector.jpg"
+      alt="First slide"
+    />
+  </Carousel.Item>
+  </Carousel>
+  </>
+    ); 
+  } else {
+    return(
+      <> 
+        <Carousel>
+        {animal?.photos.map((photo) =>{
+          return(
+          <Carousel.Item>
+            <img
+            className="d-block w-100 slide__image"
+            src={`${photo.full}`}
+            alt="First slide"
+          />
+          </Carousel.Item> 
+         );
+        })}
+        </Carousel>
+      </>
+    ); 
+  }
+  
 }
 
 export default SliderDetail
