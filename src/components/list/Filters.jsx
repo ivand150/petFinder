e293ea120b6a7f18bc99cf473a8ef5d9ea03149f
Ivan Dobry <ivandobry@hotmail.es>
2020-11-03 19:52:06 +0100

    checkbox update

--- src/components/list/Filters.jsx
+++ src/components/list/Filters.jsx
@@ -4,108 +4,96 @@ import { requestAnimals } from '../../actions/actions';
 import store from '../../stores/principal-store';
 
 function Filters({ type }) {
-  const object = {
-    age: ['any', 'young', 'baby'],
-    breed: ['Cats breed', 'Dogs breed'],
-    gender: ['female', 'male'],
-  };
+	const object = {
+		age: ['baby', 'young', 'adult', 'senior'],
+		gender: ['female', 'male', 'unknown']
+	};
 
-  let urlString = {
-    age: '',
-    breed: '',
-    gender: '',
-  };
+	let urlString = {
+		age: '',
+		breed: '',
+		gender: ''
+	};
 
-  return (
-    <>
-      <section className="filter-container">
-        <div className="horizontal-container">
-          <div id="filter-age" className="filter">
-            {object.age &&
-              object.age.map((option) => {
-                return (
-                  <label>
-                    <input
-                      type="checkbox"
-                      key={option}
-                      value={option}
-                      onChange={(event) => {
-                        urlString.age = event.target.checked
-                          ? urlString.age + `${event.target.value},`
-                          : urlString.age.replace(`${event.target.value},`, '');
-                      }}
-                    />
-                    {option}
-                  </label>
-                );
-              })}
-          </div>
+	return (
+		<>
+			<section className="filter-container">
+				<div className="horizontal-container">
+					<p>Age</p>
+					<ul class="ks-cboxtags">
+						{object.age &&
+							object.age.map((option) => {
+								return (
+									<li className="checkbox-li">
+										<input
+											type="checkbox"
+											id="checkboxOne"
+											className="inputBox"
+											key={option}
+											value={option}
+											onChange={(event) => {
+												urlString.age = event.target.checked
+													? urlString.age + `${event.target.value},`
+													: urlString.age.replace(`${event.target.value},`, '');
+											}}
+										></input>
+										<label for="checkboxOne">{option}</label>
+									</li>
+								);
+							})}
+					</ul>
 
-					<div id='filter-breed' className='filter'>
-						{object.breed &&
-							object.breed.map((option) => {
+					<div id="filter-gender" className="filter">
+						<p>Gender</p>
+						{object.gender &&
+							object.gender.map((option) => {
 								return (
 									<label>
 										<input
-											type='checkbox'
+											type="checkbox"
 											key={option}
 											value={option}
+											onChange={(event) => {
+												urlString.gender = event.target.checked
+													? urlString.gender + `${event.target.value},`
+													: urlString.gender.replace(
+															`${event.target.value},`,
+															''
+													  );
+											}}
 										/>
 										{option}
 									</label>
 								);
 							})}
 					</div>
+				</div>
 
-          <div id="filter-gender" className="filter">
-            {object.gender &&
-              object.gender.map((option) => {
-                return (
-                  <label>
-                    <input
-                      type="checkbox"
-                      key={option}
-                      value={option}
-                      onChange={(event) => {
-                        urlString.gender = event.target.checked
-                          ? urlString.gender + `${event.target.value},`
-                          : urlString.gender.replace(
-                              `${event.target.value},`,
-                              ''
-                            );
-                      }}
-                    />
-                    {option}
-                  </label>
-                );
-              })}
-          </div>
-        </div>
-        <button
-          className="button-apply"
-          onClick={() => {
-            for (const property in urlString) {
-              urlString[property] = store.removeLastComma(urlString[property]);
-            }
-            console.log('Aply filters clic');
-            requestAnimals(
-              type,
-              urlString.breed,
-              urlString.gender,
-              urlString.age
-            );
-            window.history.replaceState(
-              null,
-              '',
-              `/list?type=${type}&breed=${urlString.breed}&age=${urlString.age}&gender=${urlString.gender}`
-            );
-          }}
-        >
-          Aply filters
-        </button>
-      </section>
-    </>
-  );
+				<button
+					className="button-apply"
+					onClick={() => {
+						for (const property in urlString) {
+							urlString[property] = store.removeLastComma(urlString[property]);
+						}
+						console.log('Aply filters clic');
+						requestAnimals(
+							type,
+							urlString.breed,
+							urlString.gender,
+							urlString.age
+						);
+						window.history.replaceState(
+							null,
+							'',
+							`/list?type=${type}&breed=${urlString.breed}&age=${urlString.age}&gender=${urlString.gender}`
+						);
+					}}
+				>
+					Aply filters
+				</button>
+			</section>
+		</>
+	);
 }
 
 export default Filters;
--- src/stores/principal-store.js
+++ src/stores/principal-store.js
@@ -1,82 +1,82 @@
-import { EventEmitter } from "events";
-import dispatcher from "../dispatcher/dispatcher";
-import actionTypes from "../actions/action-types";
+import { EventEmitter } from 'events';
+import dispatcher from '../dispatcher/dispatcher';
+import actionTypes from '../actions/action-types';
 
-const CHANGE = "CHANGE";
+const CHANGE = 'CHANGE';
 let _animal;
 let _token =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJEMFZNYzJ3a0tVTEVkNzNtQkpoSWhWazdqaVUxcXgwVWpXbnFuWWZQTmRrdWMxT0VRUiIsImp0aSI6IjBhOTNkNWMxNTZlMzVlNzIwZjczZmJmNGU1MDVhODZjNjY4OTNjNjJlNTI2ZjExY2FmYjViNzVmNTEzMWUxMWUzNzYzZDg5OWVjZjNiZjg0IiwiaWF0IjoxNjA0Mzk0Nzk3LCJuYmYiOjE2MDQzOTQ3OTcsImV4cCI6MTYwNDM5ODM5Niwic3ViIjoiIiwic2NvcGVzIjpbXX0.se772Ph371Q4spcPnDx0HligGlQldbihrHtdhkHykWC9h1M28YpMD8ejJwcYowebw54fItfp6uzoXDnO2Y96FyX45wnKD29stBByD96je66uxXLCOrqlGPwKFuzHXwQErU9qFyKyD6Zd6VP2vBreLGJqeqfHHoQkANs4VntLL3EO9XF9k8lIAKQIxe0z88huIJ8ZaHQndff1gZdHt_wNkuHVptwkj-fMbJKGqMwamRX0GVy9jjrwubUMdzebefkd_Sojdt7B_ZjmQiD9x3rRcmRbX96RCmY3VB7uFoCgEa3TGXj4CY8q0qPG0ePO4cEdxfX4vF3Kb7lrmnO0dFLzwA";
+	'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIwd0NobVB0UjdWd0hHbkNEQVF0WGVUWEt3ZlZyWjNvVHhXQU5Nc3dERG1Tc1FqN05HdSIsImp0aSI6IjZhZGI1NTZmNzkxZGMzMTQ1Y2UwOTE3MGVjNTc4ZGIyZjI5Nzg3NDE5YjA2ZTdiNmIxN2ZlODA2YzQ2NWU2OWRhMTljNzcxZDE2ZWZkMDM2IiwiaWF0IjoxNjA0NDIyMzc3LCJuYmYiOjE2MDQ0MjIzNzcsImV4cCI6MTYwNDQyNTk3Nywic3ViIjoiIiwic2NvcGVzIjpbXX0.iEF61b3MkXOgY3EWgXllZ-t-WMMfU2BfEgNZiqGaLptuGeEXwfAeN9wYoTeCxlvO0NSTnSr78TGjCEGQi0wg0nNkvmUjGR3nHUDUcfw2FBPoanG_4-hSJs-vLQhL7nPs9nSh4eoIltT9rA29n5OQM_k9I4Gi-jPzA2MgOUXGp--7u_TeEVh-fDq2mppVZyUIE96A27Ob3HOyDEBCeM0ODJAwHMMJzXbRUE6-uKq0uocLeNahPM2lIVrBcaQTeZOAt5vwNgeDVMOBt_Ki_fAsHDPtDKZ3fXbDgEY_wxonUNP8CoP747Aksr9DqcAfB_wh0qC3WgjJ9oBvbnakEjJS4Q';
 let _animals = [];
 let _test;
 
 class Store extends EventEmitter {
-  setToken(token) {
-    _token = token;
-  }
+	setToken(token) {
+		_token = token;
+	}
 
-  getToken() {
-    return _token;
-  }
+	getToken() {
+		return _token;
+	}
 
-  setAnimal(animal) {
-    _animal = animal;
-  }
+	setAnimal(animal) {
+		_animal = animal;
+	}
 
-  getAnimal() {
-    return _animal;
-  }
+	getAnimal() {
+		return _animal;
+	}
 
-  setAnimals(animals) {
-    _animals = animals;
-  }
+	setAnimals(animals) {
+		_animals = animals;
+	}
 
-  getAnimals() {
-    return _animals;
-  }
+	getAnimals() {
+		return _animals;
+	}
 
-  removeLastComma(str) {
-    return str.slice(-1) === "," ? str.slice(0, str.length - 1) : str;
-  }
+	removeLastComma(str) {
+		return str.slice(-1) === ',' ? str.slice(0, str.length - 1) : str;
+	}
 
-  addEventListener(callback) {
-    this.on(CHANGE, callback);
-  }
+	addEventListener(callback) {
+		this.on(CHANGE, callback);
+	}
 
-  removeEventListener(callback) {
-    this.removeListener(CHANGE, callback);
-  }
+	removeEventListener(callback) {
+		this.removeListener(CHANGE, callback);
+	}
 
-  emitChange() {
-    this.emit(CHANGE);
-  }
+	emitChange() {
+		this.emit(CHANGE);
+	}
 
-  getTestVar() {
-    return _test;
-  }
+	getTestVar() {
+		return _test;
+	}
 }
 
 const store = new Store();
 
 dispatcher.register((action) => {
-  switch (action.type) {
-    case actionTypes.REQUEST_TOKEN:
-      _token = action.payload;
-      store.emitChange();
-      break;
-    case actionTypes.REQUEST_ANIMAL:
-      _animal = action.payload;
-      store.emitChange();
-      break;
-    case actionTypes.REQUEST_ANIMALS:
-      _animals = action.payload;
-      store.emitChange();
-      break;
+	switch (action.type) {
+		case actionTypes.REQUEST_TOKEN:
+			_token = action.payload;
+			store.emitChange();
+			break;
+		case actionTypes.REQUEST_ANIMAL:
+			_animal = action.payload;
+			store.emitChange();
+			break;
+		case actionTypes.REQUEST_ANIMALS:
+			_animals = action.payload;
+			store.emitChange();
+			break;
 
-    default:
-      _test = "break";
-      store.emitChange();
-      break;
-  }
+		default:
+			_test = 'break';
+			store.emitChange();
+			break;
+	}
 });
 
 export default store;
