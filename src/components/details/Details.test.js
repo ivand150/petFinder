d3e8ee0207458a304a5e3eff44fa2152c6cb37ce
Ivan Dobry <ivandobry@hotmail.es>
2020-11-04 10:55:39 +0100

    testing details

--- src/components/details/Details.test.js
+++ src/components/details/Details.test.js
@@ -1,16 +1,53 @@
-import { render, screen } from "@testing-library/react";
-import Details from "./Details";
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import Details from './Details';
 
-test("renders learn react link", () => {
-  render(
-    <Details
-      animal={{
-        description: "test",
-        breeds: { primary: "" },
-        tags: ["1", "2"],
-      }}
-    />
-  );
-  const linkElement = screen.getByText(/test/i);
-  expect(linkElement).toBeInTheDocument();
+describe.only('details', () => {
+	let container;
+
+	beforeEach(() => {
+		container = document.createElement('div');
+		document.body.appendChild(container);
+	});
+
+	afterEach(() => {
+		unmountComponentAtNode(container);
+		container.remove();
+		container = null;
+	});
+
+	test('should render details', () => {
+		act(() => {
+			render(
+				<Details
+					animal={{
+						name: 'ozzy',
+						description: 'test',
+						breeds: { primary: '' },
+						tags: ['1', '2']
+					}}
+				/>,
+				container
+			);
+		});
+		console.log(container.textContent);
+		expect(container.querySelector('#donating').textContent).toBe('Donate!');
+		expect(container.querySelector('#animal__name').textContent).toBe('ozzy');
+	});
+});
+
+test('renders learn react link', () => {
+	render(
+		<Details
+			animal={{
+				description: 'test',
+				breeds: { primary: '' },
+				tags: ['1', '2']
+			}}
+		/>
+	);
+	const linkElement = screen.getByText(/test/i);
+	expect(linkElement).toBeInTheDocument();
 });
--- src/components/footer/Footer.test.js
+++ src/components/footer/Footer.test.js
@@ -2,9 +2,9 @@ import Footer from './Footer';
 import { render, screen } from '@testing-library/react';
 
 describe('Footer', () => {
-  test('should render', () => {
-    render(<Footer />);
-    const linkElement = screen.getByText(/Pet Finder/i);
-    expect(linkElement).toBeInTheDocument();
-  });
+	test('should render', () => {
+		render(<Footer />);
+		const linkElement = screen.getByText(/Pet Finder/i);
+		expect(linkElement).toBeInTheDocument();
+	});
 });
