e175bc592f8516b96b2993d76bc8fb2437e9e299
Darina Rybalchenko <dari.ryb@gmail.com>
2020-11-04 14:51:30 +0100

    footer

--- src/components/footer/Footer.test.js
+++ src/components/footer/Footer.test.js
@@ -5,7 +5,6 @@ import { act } from 'react-dom/test-utils'
 import { screen } from '@testing-library/react';
 
 describe('Footer', () => {
-<<<<<<< HEAD
   let container;
   beforeEach(() => {
     container = document.createElement('div');
@@ -33,12 +32,5 @@ describe('Footer', () => {
     const linkElement = screen.getByText(/Contact Us/i);
     expect(linkElement).toBeInTheDocument();
   });
-=======
-	test('should render', () => {
-		render(<Footer />);
-		const linkElement = screen.getByText(/Pet Finder/i);
-		expect(linkElement).toBeInTheDocument();
-	});
->>>>>>> d5aceb369296c2447bf27c98fd8cd5e3e8e360ef
 });
 
