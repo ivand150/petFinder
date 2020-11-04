ee6b95bc1a28de831b7a52ab3297a108e9f85ad4
Darina Rybalchenko <dari.ryb@gmail.com>
2020-11-04 14:39:18 +0100

    add footer test

--- src/components/list/list.test.js
+++ src/components/list/list.test.js
@@ -1,12 +1,29 @@
+import React from 'react'
 import List from './List'
-import { render, screen } from '@testing-library/react'
+import { unmountComponentAtNode, render } from 'react-dom';
+import { act } from 'react-dom/test-utils'
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-    render(<List animals={[
-        { name: 'Kitty', photos: [{ medium: '' }], breeds: { primary: '' } }
-    ]}
-    />);
-    const linkElement = screen.getByText(/Kitty/i);
-    expect(linkElement).toBeInTheDocument();
-});
+describe('List', () => {
+    let container;
+    beforeEach(() => {
+        container = document.createElement('div')
+        document.body.appendChild(container);
+    })
 
+    afterEach(() => {
+        unmountComponentAtNode(container)
+        container.remove()
+        container = null
+    })
+
+    test('should render', () => {
+        act(() => {
+            render(<BrowserRouter> <List animals={[
+                { name: 'Kitty', photos: [{ medium: '' }], breeds: { primary: '' } }]}
+            /></BrowserRouter>, container);
+        })
+        expect(container.querySelector('[data-testid="name"]').textContent).toBe('Kitty')
+    })
+
+})
